bfb61681a0954fd098625fa80d691ef734776c8b
Siddharth <siddhusreeni@gmail.com>
2024-11-07 17:07:29 +0530

    folder structure added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Page1 from './pages/Page1'
+
+const App = () => {
+  return (
+    <>
+      <Page1 />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Page1.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Page1 = () => {
+  return (
+    <div className='h-screen p-10 bg-white'>
+        <div className="h-full w-full">
+            h2ello
+        </div>
+
+    </div>
+  )
+}
+
+export default Page1
