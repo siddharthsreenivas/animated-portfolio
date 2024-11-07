ccf8a9328df0c08c45a0985a2f3283f5d04d26e9
Siddharth <siddhusreeni@gmail.com>
2024-11-07 20:15:06 +0530

    page 2 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Page1 from "./pages/Page1";
 import Header from "./components/Header";
+import Page2 from "./pages/Page2";
+import Page3 from "./pages/Page3";
 
 const App = () => {
   return (
@@ -8,6 +10,8 @@ const App = () => {
       <div id="main">
         <Header />
         <Page1 />
+        <Page2 />
+        <Page3 />
       </div>
     </>
   );
